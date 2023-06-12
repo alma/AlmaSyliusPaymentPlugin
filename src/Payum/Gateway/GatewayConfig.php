1a459e0cfcfc0134f1735551ac2eb2cbc42670e9
jb cr <51637606+jbcr@users.noreply.github.com>
2023-06-12 15:38:25 +0200

    fix get gateway name

--- src/Payum/Gateway/GatewayConfig.php
+++ src/Payum/Gateway/GatewayConfig.php
@@ -22,7 +22,7 @@ class GatewayConfig implements GatewayConfigInterface
 
     public function getFactoryName(): string
     {
-        return array_key_exists('payum.factory_name', $this->config) ?
+        return $this->config->offsetExists('payum.factory_name') ?
             $this->config['payum.factory_name'] : '';
     }
 
